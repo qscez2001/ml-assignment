db5b6aba3096218ee196a194370cbf9d21e78417
Nicole <nicole.chen@ubitus.net>
2024-02-17 18:10:36 +0800

    implement input and output model format

--- app/main.py
+++ app/main.py
@@ -1,10 +1,39 @@
+from typing import List
+from pydantic import BaseModel
 from fastapi import FastAPI
+# from translation_example import translate
 
 app = FastAPI()
 
+class Record(BaseModel):
+    id: str
+    text: str
 
+class TranslationPayload(BaseModel):
+    fromLang: str
+    records: List[Record]
+    toLang: str
 
+class Payload(BaseModel):
+    payload: TranslationPayload
 
-@app.post("/translation")
-def translation(text: str,source_language: str,  target_language: str):
-    return {"result": "人生はチョコレートの箱のようなものだ。"}
+class TranslatedRecord(BaseModel):
+    id: str
+    text: str
+
+class TranslationResult(BaseModel):
+    result: List[TranslatedRecord]
+    
+@app.post("/translation", response_model=TranslationResult)
+def translation(payload: Payload):
+    # print(payload)
+    # payload = payload.payload
+    # print(payload)
+    # records = payload.records
+    # for record in records:
+    #     print(record.text)
+        
+        # record.text = translate(record.text, payload.fromLang, payload.toLang)
+    translated_record = TranslatedRecord(id="123", text="人生はチョコレートの箱のようなものだ。")
+    return TranslationResult(result=[translated_record])
+    # return {"result": "人生はチョコレートの箱のようなものだ。"}
--- app/translation_example.py
+++ app/translation_example.py
@@ -2,8 +2,17 @@ from transformers import M2M100Config, M2M100ForConditionalGeneration, M2M100Tok
 
 
 model = M2M100ForConditionalGeneration.from_pretrained('facebook/m2m100_418M')
-tokenizer = M2M100Tokenizer.from_pretrained('facebook/m2m100_418M', src_lang="en", tgt_lang="fr")
+tokenizer = M2M100Tokenizer.from_pretrained('facebook/m2m100_418M', src_lang="en", tgt_lang="jp")
 
+
+def translate(src_text: str, src_lang:str, tgt_lang: str) -> str:
+    tokenizer.src_lang = src_lang
+    
+    model_inputs = tokenizer(src_text, return_tensors="pt")
+    generated_tokens = model.generate(**model_inputs, forced_bos_token_id=tokenizer.get_lang_id(tgt_lang))
+    result = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+
+    return result
 
 if __name__ == "__main__":
 
